b579d3c55cbb5d79baa47feec3a27c01df6e19e8
funMango <bluemango608@gmail.com>
2025-08-10 11:27:12 +0900

    first commit

---
+++ PasswordBox/PasswordBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordBoxApp.swift
+//  PasswordBox
+//
+//  Created by 이민호 on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PasswordBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
